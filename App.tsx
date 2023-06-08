60c838364e00e0361d05d85af9e4be1027e364ac
IVT202ipp <dg_gd_84@mail.ru>
2023-06-08 16:10:02 +0900

    image

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Alert, FlatList } from 'react-native';
+import { View, Text, Alert, FlatList, StyleSheet, Image } from 'react-native';
 
 function App() {
   const [items, setitems] = React.useState([]);
@@ -18,9 +18,10 @@ function App() {
 
   React.useEffect(fetchPosts, []);
 
-  const Post = ({ title, desc }: { title: any, desc: any }) => {
+  const Post = ({ title, desc, img }: { title: any, desc: any, img: any }) => {
     return (
-      <View>
+      <View style={styles.container}>
+        <Image source={{ uri: img }} style={styles.image} />
         <Text>{title}</Text>
         <Text>{desc}</Text>
       </View>
@@ -31,10 +32,23 @@ function App() {
     <View>
       <FlatList
         data={items}
-        renderItem={({ item }) => <Post title={item.title} desc={item.description} />}
+        renderItem={({ item }) => <Post title={item.title} desc={item.description} img={item.thumbnail} />}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 8,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginRight: 12,
+  },
+});
 
 export default App;
