72a181efcde9e691327d75cc0b81c69397c3f0c2
IVT202ipp <dg_gd_84@mail.ru>
2023-06-12 17:09:51 +0900

    Update Rate.tsx

--- components/Rate.tsx
+++ components/Rate.tsx
@@ -2,26 +2,29 @@ import React from 'react';
 import { View, Image } from 'react-native';
 
 const Rate = ({ Rating } : { Rating:any }) => {
-  const renderStars = () => {
+    const renderStars = () => {
     const stars = [];
+    if (Rating-Math.floor(Rating) >= 0.8) Rating = 5;
     for (let i = 1; i <= 5; i++) {
-      const starColor = i <= Rating ? 'yellow' : 'gray';
-      stars.push(
-        <Image
-          key={i}
-          source={require('./star.png')}
-          style={{ width: 24, height: 24, tintColor: starColor }}
-        />
-      );
-    }
-    return stars;
-  };
+        
+        const starColor = i <= Rating ? 'yellow' : 'gray';
+        
+        stars.push(
+            <Image
+                key={i}
+                source={require('./star.png')}
+                style={{ width: 24, height: 24, tintColor: starColor }}
+            />
+        );
+        }
+        return stars;
+    };
 
-  return (
-  <View style={{ flexDirection: 'row' }}>
-    {renderStars()}
-  </View>
-  );
+    return (
+        <View style={{ flexDirection: 'row' }}>
+            {renderStars()}
+        </View>
+    );
 };
 
 export default Rate;
