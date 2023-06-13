bca7ef2ce3e919d79d6ea7eceef07496609b9754
IVT202ipp <dg_gd_84@mail.ru>
2023-06-13 17:29:10 +0900

    еуые

--- App.tsx
+++ App.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { Navigation } from './components/Navigation';
 
 function App() {
-  //<Navigation />
+  return <Navigation />;
 }
 export default App;
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,5 +1,15 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { Home } from "./Home"
+import { NavigationContainer } from "@react-navigation/native";
 
+const Stack = createNativeStackNavigator();
 
 export const Navigation = () => {
-
-}
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={Home} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
