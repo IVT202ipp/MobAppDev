2eafb83b75349ffd2937713afdf06e8227d451a1
IVT202ipp <dg_gd_84@mail.ru>
2023-06-23 14:45:02 +0900

    colorchange

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { Animated, StyleSheet } from 'react-native';
 
 export const Profile = () => {
+  const backgroundColor = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(backgroundColor, {
+          toValue: 1,
+          duration: 1000,
+          useNativeDriver: false,
+        }),
+        Animated.timing(backgroundColor, {
+          toValue: 0,
+          duration: 1000,
+          useNativeDriver: false,
+        }),
+      ])
+    ).start();
+  }, []);
+
+  const bg = backgroundColor.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['red', 'blue'],
+  });
+
   return (
-    <View>
-    </View>
+      <Animated.View style={[styles.container, { backgroundColor: bg }]} />
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
